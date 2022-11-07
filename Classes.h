76f63aa3a746f15967be3e6c03d975d1cc754d9a
dan <banceadan20@gmail.com>
2022-11-07 17:44:55 +0200

    test

---
+++ Classes.h
@@ -0,0 +1,8 @@
+//
+// Created by dan on 11/14/2022.
+//
+
+#ifndef OOP_CLASSES_H
+#define OOP_CLASSES_H
+
+#endif //OOP_CLASSES_H
--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 
+
+
+
+
+
 int main() {
     std::cout << "Hello, world!\n";
     std::cout << "test";
     std::cout << "test2";
     return 0;
+
 }
